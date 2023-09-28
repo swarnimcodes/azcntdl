a985ed1a30e259c9b317725119187e4d108cd60a
Swarnim B - MasterSoft <swarnim14.9@hotmail.com>
2023-09-28 10:54:22 +0530

    working program!!!

--- azcntdl.py
+++ azcntdl.py
@@ -1,14 +1,46 @@
 from azure.storage.blob import BlobServiceClient
+from azure.identity import ClientSecretCredential
+import os
 
-account_name = "rfcappstorage"
-account_key = "si=obecontainer-policy&spr=https&sv=2021-12-02&sr=c&sig=Fm3IjNp%2BiQDfKSlgdoGyqhyLmpVtMF10xRtWB2ZRCWs%3D"
+# Replace with your Azure Blob Storage credentials
+storage_account_name = "rfcappstorage"
+tenant_id = "b31c6b9d-6ab4-4c0a-9b86-94f76df3f85b"
+client_id = "94ef8877-3389-4c51-ad69-e0cb52d9c965"
+client_secret = "IN98Q~39SDJqNw5xbzL92RsZvo1qIdWfJD1nUc0R"
+dl_path = "./dldump"
 
-bsc = BlobServiceClient(
-    account_url = f"https://{account_name}.blob.core.windows.net",
-    credential=account_key
+# Create a credential using your application's client id, client secret, and tenant id
+credential = ClientSecretCredential(
+    tenant_id=tenant_id,
+    client_id=client_id,
+    client_secret=client_secret
 )
 
+# Create a BlobServiceClient using the storage account name and the credential
+bsc = BlobServiceClient(
+    account_url=f"https://{storage_account_name}.blob.core.windows.net",
+    credential=credential
+)
+
+os.makedirs(dl_path, exist_ok=True)
+
+# List all containers in the storage account
 containers = bsc.list_containers()
 
+# Print the names of all containers
 for container in containers:
-    print(container)
+    print(f"Processing Container:\t{container.name}")
+    container_client = bsc.get_container_client(container.name)
+    blob_list = container_client.list_blobs()
+        
+    for blob in blob_list:
+        print(f"Downloading Blob:\t{blob.name}")
+        blob_client = container_client.get_blob_client(blob.name)
+        dl_file_path = os.path.join(dl_path, blob.name)
+        os.makedirs(os.path.dirname(dl_file_path), exist_ok=True)
+        with open(dl_file_path, 'wb') as dl_file:
+            dl_file.write(blob_client.download_blob().readall())
+
+        print(f"Downloaded Blob {blob.name} to {dl_file_path}")
+    
+print("All data downloaded.")        
