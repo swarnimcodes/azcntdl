ef1f22e297ed3fcd3de0c3b5a4e5778e5a0d96c3
Swarnim B - MasterSoft <swarnim14.9@hotmail.com>
2023-09-28 11:21:15 +0530

    refactored code and added logger

--- azcntdl.py
+++ azcntdl.py
@@ -1,46 +1,86 @@
+import os
+import yaml
 from azure.storage.blob import BlobServiceClient
 from azure.identity import ClientSecretCredential
-import os
+import datetime
 
-# Replace with your Azure Blob Storage credentials
-storage_account_name = "rfcappstorage"
-tenant_id = "b31c6b9d-6ab4-4c0a-9b86-94f76df3f85b"
-client_id = "94ef8877-3389-4c51-ad69-e0cb52d9c965"
-client_secret = "IN98Q~39SDJqNw5xbzL92RsZvo1qIdWfJD1nUc0R"
-dl_path = "./dldump"
+# Load the configuration from the YAML file
+with open("config.yaml", "r") as config_file:
+    config = yaml.safe_load(config_file)
 
-# Create a credential using your application's client id, client secret, and tenant id
-credential = ClientSecretCredential(
-    tenant_id=tenant_id,
-    client_id=client_id,
-    client_secret=client_secret
-)
+# Custom Logging
+def logger(loglevel: str, message: str) -> None:
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    logfile_path = "azcntdl.log"
+    logmsg = f"{timestamp}  -  {loglevel}  -  {message}"
+    with open(logfile_path, 'a') as logf:
+        logf.write(f"{logmsg}\n")
 
-# Create a BlobServiceClient using the storage account name and the credential
-bsc = BlobServiceClient(
-    account_url=f"https://{storage_account_name}.blob.core.windows.net",
-    credential=credential
-)
+def main():
+    # Iterate through each configuration
+    for account_config in config:
+        storage_account_name = account_config["storage_account_name"]
+        tenant_id = account_config["tenant_id"]
+        client_id = account_config["client_id"]
+        client_secret = account_config["client_secret"]
+        dl_path = account_config["dl_path"]
 
-os.makedirs(dl_path, exist_ok=True)
+        # Create a credential using the current account's client id, client secret, and tenant id
+        credential = ClientSecretCredential(
+            tenant_id=tenant_id,
+            client_id=client_id,
+            client_secret=client_secret
+        )
 
-# List all containers in the storage account
-containers = bsc.list_containers()
+        # Create a BlobServiceClient using the current storage account's name and the credential
+        bsc = BlobServiceClient(
+            account_url=f"https://{storage_account_name}.blob.core.windows.net",
+            credential=credential
+        )
 
-# Print the names of all containers
-for container in containers:
-    print(f"Processing Container:\t{container.name}")
-    container_client = bsc.get_container_client(container.name)
-    blob_list = container_client.list_blobs()
-        
-    for blob in blob_list:
-        print(f"Downloading Blob:\t{blob.name}")
-        blob_client = container_client.get_blob_client(blob.name)
-        dl_file_path = os.path.join(dl_path, blob.name)
-        os.makedirs(os.path.dirname(dl_file_path), exist_ok=True)
-        with open(dl_file_path, 'wb') as dl_file:
-            dl_file.write(blob_client.download_blob().readall())
+        os.makedirs(dl_path, exist_ok=True)
 
-        print(f"Downloaded Blob {blob.name} to {dl_file_path}")
-    
-print("All data downloaded.")        
+        # List all containers in the storage account
+        containers = bsc.list_containers()
+
+        # Print the names of all containers for the current storage account
+        for container in containers:
+            print(f"Processing Container ({storage_account_name}):\t{container.name}")
+            logger(
+                loglevel = "INFO",
+                message = f"Processing Container ({storage_account_name}):\t{container.name}"
+            )
+            container_client = bsc.get_container_client(container.name)
+            blob_list = container_client.list_blobs()
+
+            for blob in blob_list:
+                print(f"Downloading Blob ({storage_account_name}):\t{blob.name}")
+                logger(
+                    loglevel = "INFO",
+                    message = f"Downloading Blob ({storage_account_name}):\t{blob.name}"
+                )
+                blob_client = container_client.get_blob_client(blob.name)
+                dl_file_path = os.path.join(dl_path, blob.name)
+                os.makedirs(os.path.dirname(dl_file_path), exist_ok=True)
+                with open(dl_file_path, 'wb') as dl_file:
+                    dl_file.write(blob_client.download_blob().readall())
+
+                print(f"Downloaded Blob {blob.name} to {dl_file_path}")
+                logger(
+                    loglevel = "INFO",
+                    message = f"Downloaded Blob {blob.name} to {dl_file_path}"
+                )
+
+    print("All data downloaded.")
+    logger(
+        loglevel = "INFO",
+        message = "All data downloaded!"
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+# TODO: resume partial downloads
+# TODO: log file functionality
+# TODO: mailing functionality
